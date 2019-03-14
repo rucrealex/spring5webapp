3ed6e233fcddacbbbb32b6c28b4bcaa9ce343ad4
rucrealex <rucrealex@gmail.com>
2019-03-14 22:19:21 +0700

    add publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,10 +2,11 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PubisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class DevBootstrap  {
 
-    private AuthorRepository authorRepository;
-    private BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PubisherRepository pubisherRepository;
 
     @Autowired
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PubisherRepository pubisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.pubisherRepository = pubisherRepository;
     }
 
     @EventListener
@@ -29,22 +32,26 @@ public class DevBootstrap  {
 
     private void initData() {
         Author robert = new Author("Robert", "Gelbraith");
-        Book book1 = new Book("The cuckoo's calling", "1-234234", "Attics");
-        Book book2 = new Book("Career of Devil", "1-234-124", "Attics");
+        Publisher attics = new Publisher("Attics", "NY Five Avenue str, 5");
+        Book book1 = new Book("The cuckoo's calling", "1-234234", attics);
+        Book book2 = new Book("Career of Devil", "1-234-124", attics);
         robert.getBooks().add(book1);
         robert.getBooks().add(book2);
         book1.getAuthors().add(robert);
         book2.getAuthors().add(robert);
 
+        pubisherRepository.save(attics);
         authorRepository.save(robert);
         bookRepository.save(book1);
         bookRepository.save(book2);
 
         Author rod = new Author("Rod", "Johnson");
-        Book book3 = new Book("J2EE Development without EJB", "12334", "Corona");
+        Publisher corona = new Publisher("Corona", "LA, Union square, 7-12");
+        Book book3 = new Book("J2EE Development without EJB", "12334", corona);
         rod.getBooks().add(book3);
         book3.getAuthors().add(rod);
 
+        pubisherRepository.save(corona);
         authorRepository.save(rod);
         bookRepository.save(book3);
     }
